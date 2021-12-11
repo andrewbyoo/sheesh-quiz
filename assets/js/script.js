f7edc2a188ebaa03a271d837c1da379b68257043
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-11 17:31:29 -0500

    Removed code that will not be used, changed variables to match updated HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,23 @@
 // Define variables for html elements
 var startPage = document.getElementById("startPage");
 var startButton = document.getElementById("startButton");
-var quizItems = document.getElementsByClassName("quizItems");
-var questions = document.getElementsByClassName("question");
-var answers = document.getElementsByClassName("answers");
-var q1 = document.getElementById("q1");
-var q2 = document.getElementById("q2");
-var q3 = document.getElementById("q3");
-var q4 = document.getElementById("q4");
-var q5 = document.getElementById("q5");
-var correct = document.querySelector(".correct");
-var incorrect = document.querySelector(".incorrect");
+var quizContainer = document.getElementById("quizContainer");
+var questions = document.getElementById("questions");
+var answers = document.getElementById("answers");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
 var endPage = document.getElementById("endPage");
 var clearBoard = document.getElementById("scoreClear");
 
 var startPageMode = "visible"
-var grandParentMode = ""
-
 
 startButton.addEventListener("click", function() {
 
   if (startPageMode === "visible") {
     startPageMode = "hidden"
     startPage.setAttribute("class", "hidden");
-    q1.setAttribute("class", "visible");
-  }
-})
-
-incorrect.addEventListener("click", function() {
-
-  grandParentId = this.parentElement.parentElement.parentElement.id
-  console.log(grandParentId)
-
-  if (grandParentId === "q1") {
-
-    grandParentMode = this.parentElement.parentElement.parentElement.className
-    console.log(grandParentMode)
-
-    if (grandParentMode === "visible") {
-      q1.setAttribute("class", "hidden");
-      q2.setAttribute("class", "visible");
-    }
-  }
-
-  if (grandParentId === "q2") {
-
-    grandParentMode = this.parentElement.parentElement.parentElement.className
-    console.log(grandParentMode)
-
-    if (grandParentMode === "visible") {
-      q2.setAttribute("class", "hidden");
-      q3.setAttribute("class", "visible");
-    }
+    .setAttribute("class", "visible");
   }
 })
