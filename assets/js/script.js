b2afd59c3aff6a4bfc645e8364a3458950c10bbd
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-11 13:57:15 -0500

    Added variables for the start/restart buttons and scoreboard clear button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Define variables for html elements
+var startButton = document.getElementsByClassName("startButton");
+var quizItems = document.getElementsByClassName("quizItems");
 var questions = document.getElementsByClassName("question");
 var answers = document.getElementsByClassName("answers");
-var quizItems = document.getElementsByClassName("quizItems");
 var q1 = document.getElementById("q1");
 var q2 = document.getElementById("q2");
 var q3 = document.getElementById("q3");
@@ -9,3 +10,4 @@ var q4 = document.getElementById("q4");
 var q5 = document.getElementById("q5");
 var correct = document.getElementsByClassName("correct");
 var incorrect = document.getElementsByClassName("incorrect");
+var clearBoard = document.getElementById("scoreClear")
