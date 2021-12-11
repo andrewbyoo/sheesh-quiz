f4ec09cb2e9e918b4ce2a87c3af93099dfe6ac79
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-10 19:48:03 -0500

    Defined variables for html elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Define variables for html elements
+var questions = document.getElementsByClassName("question");
+var answers = document.getElementsByClassName("answers");
+var quizItems = document.getElementsByClassName("quizItems");
+var q1 = document.getElementById("q1");
+var q2 = document.getElementById("q2");
+var q3 = document.getElementById("q3");
+var q4 = document.getElementById("q4");
+var q5 = document.getElementById("q5");
+var correct = document.getElementsByClassName("correct");
+var incorrect = document.getElementsByClassName("incorrect");
