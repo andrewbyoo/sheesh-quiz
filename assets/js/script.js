7422aeecca3cbeca26c27acacedc3e2b79014f55
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-11 21:01:10 -0500

    Added variable for correct and incorrect html id's, changed correct answer in the objects for testing, added if and else statements in each answer button to hide and show the correct and incorrect message

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
 var btn4 = document.getElementById("btn4");
+var correct = document.getElementById("correct");
+var incorrect = document.getElementById("incorrect");
 var endPage = document.getElementById("endPage");
 var clearBoard = document.getElementById("scoreClear");
 var i = 0;
@@ -17,17 +19,17 @@ var questionInfo = [
   {
     question: "Lorem Ipsum Question 1",
     options: ["Lorem Ipsum Q1 Answer 1", "Lorem Ipsum Q1 Answer 2", "Lorem Ipsum Q1 Answer 3", "Lorem Ipsum Q1 Answer 4"],
-    correctAnswer: "Lorem Ipsum Q1 Answer 4"
+    correctAnswer: "Lorem Ipsum Q1 Answer 1"
   },
   {
     question: "Lorem Ipsum Question 2",
     options: ["Lorem Ipsum Q2 Answer 1", "Lorem Ipsum Q2 Answer 2", "Lorem Ipsum Q2 Answer 3", "Lorem Ipsum Q2 Answer 4"],
-    correctAnswer: "Lorem Ipsum Q2 Answer 4"
+    correctAnswer: "Lorem Ipsum Q2 Answer 2"
   },
   {
     question: "Lorem Ipsum Question 3",
     options: ["Lorem Ipsum Q3 Answer 1", "Lorem Ipsum Q3 Answer 2", "Lorem Ipsum Q3 Answer 3", "Lorem Ipsum Q3 Answer 4"],
-    correctAnswer: "Lorem Ipsum Q3 Answer 4"
+    correctAnswer: "Lorem Ipsum Q3 Answer 3"
   },
   {
     question: "Lorem Ipsum Question 4",
@@ -37,7 +39,7 @@ var questionInfo = [
   {
     question: "Lorem Ipsum Question 5",
     options: ["Lorem Ipsum Q5 Answer 1", "Lorem Ipsum Q5 Answer 2", "Lorem Ipsum Q5 Answer 3", "Lorem Ipsum Q5 Answer 4"],
-    correctAnswer: "Lorem Ipsum Q5 Answer 4"
+    correctAnswer: "Lorem Ipsum Q5 Answer 1"
   }
 ]
 
@@ -50,21 +52,49 @@ startButton.addEventListener("click", function () {
 
 // Answer 1 button
 btn1.addEventListener("click", function () {
+  if (btn1.innerHTML == questionInfo[i-1].correctAnswer) {
+    correct.setAttribute("class", "visible");
+    incorrect.setAttribute("class", "hidden");
+  } else {
+    incorrect.setAttribute("class", "visible");
+    correct.setAttribute("class", "hidden");
+  }
   quiz();
 })
 
 // Answer 2 button
 btn2.addEventListener("click", function () {
+  if (btn2.innerHTML == questionInfo[i-1].correctAnswer) {
+    correct.setAttribute("class", "visible");
+    incorrect.setAttribute("class", "hidden");
+  } else {
+    incorrect.setAttribute("class", "visible");
+    correct.setAttribute("class", "hidden");
+  }
   quiz();
 })
 
 // Answer 3 button
 btn3.addEventListener("click", function () {
+  if (btn3.innerHTML == questionInfo[i-1].correctAnswer) {
+    correct.setAttribute("class", "visible");
+    incorrect.setAttribute("class", "hidden");
+  } else {
+    incorrect.setAttribute("class", "visible");
+    correct.setAttribute("class", "hidden");
+  }
   quiz();
 })
 
 // Answer 4 button
 btn4.addEventListener("click", function () {
+  if (btn4.innerHTML == questionInfo[i-1].correctAnswer) {
+    correct.setAttribute("class", "visible");
+    incorrect.setAttribute("class", "hidden");
+  } else {
+    incorrect.setAttribute("class", "visible");
+    correct.setAttribute("class", "hidden");
+  }
   quiz();
 })
 
